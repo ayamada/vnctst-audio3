16bc3e81ff3c65688832e90614717b8838fe7b8a
atsuo yamada <yamada-git@tir.jp>
2016-06-22 13:04:21 +0900

    Add github ribbon

--- src/ring/vnctst/audio3/demo/server.clj
+++ src/ring/vnctst/audio3/demo/server.clj
@@ -34,7 +34,8 @@
   [:button {:onclick onclick} label])
 
 (defn render-app [req]
-  (let [address [:p [:a {:href "https://github.com/ayamada/vnctst-audio3"
+  (let [github-url "https://github.com/ayamada/vnctst-audio3"
+        address [:p [:a {:href github-url
                          :target "_blank"}
                      "(vnctst-audio3 github repos)"]]]
     {:status 200
@@ -57,7 +58,13 @@
               ]
              [:body
               {:onload "vnctst.audio3.demo.bootstrap()"}
-              ;; TODO: github ribbon をつける
+              [:div#github-ribbon
+               [:a {:href github-url
+                    :target "_blank"}
+                [:img {:style "position: absolute; top: 0; right: 0; border: 0;"
+                       :src "https://camo.githubusercontent.com/365986a132ccd6a44c23a9169022c0b5c890c387/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f7265645f6161303030302e706e67"
+                       :alt "Fork me on GitHub"
+                       :data-canonical-src "https://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png"}]]]
               [:h1 title]
               address
               [:div#message "Loading ..."]
